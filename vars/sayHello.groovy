91c6a6fff77f79e41caef0d1d1065c82fca84a30
Ruben Dopazo <ruben@reluscloud.com>
2019-03-20 19:25:35 -0300

    testing

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -6,7 +6,7 @@ import com.cleverbuilder.*
 def call(String name = 'human') {
   GlobalVars obj = new GlobalVars(this)
   obj.send_request()
-  println send_request2()
+  println obj.send_request2()
   echo "Hello, ${GlobalVars.foo} ${name}."
 }
 
