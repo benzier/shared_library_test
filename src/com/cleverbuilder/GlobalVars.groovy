141305cfefcc4a0067c7227c10262e10acc266d1
Ruben Dopazo <ruben@reluscloud.com>
2019-03-20 19:03:47 -0300

    testing

--- src/com/cleverbuilder/GlobalVars.groovy
+++ src/com/cleverbuilder/GlobalVars.groovy
@@ -12,11 +12,35 @@ class GlobalVars {
    // println GlobalVars.foo
 
     GlobalVars(pipeline){
-        obj = pipeline
+        obj =pipeline
     }
 
    def send_request(){
-       def http = new groovyx.net.http.HTTPBuilder('http://www.codehaus.org')
-       obj.echo(http)
+
+        def http = new HTTPBuilder( 'http://ajax.googleapis.com' )
+            // perform a GET request, expecting JSON response data
+            http.request( GET, JSON ) {
+            uri.path = '/ajax/services/search/web'
+            uri.query = [ v:'1.0', q: 'Calvin and Hobbes' ]
+
+            headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
+
+            // response handler for a success response code:
+            response.success = { resp, json ->
+                obj.echo resp.status
+
+                // parse the JSON response object:
+                json.responseData.results.each {
+                obj.echo "  ${it.titleNoFormatting} : ${it.visibleUrl}"
+                }
+            }
+
+            // handler for any failure status code:
+            response.failure = { resp ->
+                obj.echo "Unexpected error: ${resp.status} : ${resp.statusLine.reasonPhrase}"
+            }
+        }
+
+
    }
 }
