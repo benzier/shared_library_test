45cb7f459f58421d669ef43d6807df658ddfa72f
Ruben Dopazo <ruben@reluscloud.com>
2019-03-20 19:08:55 -0300

    testing

--- src/com/cleverbuilder/GlobalVars.groovy
+++ src/com/cleverbuilder/GlobalVars.groovy
@@ -20,32 +20,31 @@ class GlobalVars {
         obj =pipeline
     }
 
-   def send_request(){
+    def send_request(){
 
-        def http = new HTTPBuilder( 'http://ajax.googleapis.com' )
+        def http = new HTTPBuilder( 'http://ajax.googleapis.com' ) {
             // perform a GET request, expecting JSON response data
             http.request( GET, JSON ) {
-            uri.path = '/ajax/services/search/web'
-            uri.query = [ v:'1.0', q: 'Calvin and Hobbes' ]
+                uri.path = '/ajax/services/search/web'
+                uri.query = [ v:'1.0', q: 'Calvin and Hobbes' ]
 
-            headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
+                headers.'User-Agent' = 'Mozilla/5.0 Ubuntu/8.10 Firefox/3.0.4'
 
-            // response handler for a success response code:
-            response.success = { resp, json ->
-                obj.echo resp.status
+                // response handler for a success response code:
+                response.success = { resp, json ->
+                    obj.echo resp.status
 
-                // parse the JSON response object:
-                json.responseData.results.each {
-                obj.echo "  ${it.titleNoFormatting} : ${it.visibleUrl}"
+                    // parse the JSON response object:
+                    json.responseData.results.each {
+                    obj.echo "  ${it.titleNoFormatting} : ${it.visibleUrl}"
+                    }
+                }
+
+                // handler for any failure status code:
+                response.failure = { resp ->
+                    obj.echo "Unexpected error: ${resp.status} : ${resp.statusLine.reasonPhrase}"
                 }
             }
-
-            // handler for any failure status code:
-            response.failure = { resp ->
-                obj.echo "Unexpected error: ${resp.status} : ${resp.statusLine.reasonPhrase}"
-            }
         }
-
-
-   }
+    }
 }
