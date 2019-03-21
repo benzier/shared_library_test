093802dd7f8fbcb437f388acae9162f9fa7a2bfb
Ruben Dopazo <ruben@reluscloud.com>
2019-03-21 14:23:36 -0300

    test

--- vars/sayHello.groovy
+++ vars/sayHello.groovy
@@ -6,7 +6,7 @@ import com.p72.devops.*
 def call(String name = 'human') {
     //GlobalVars obj = new GlobalVars(this)
     RestClient client = new RestClient();
-    def var = client.getApiCall([], "https://jsonplaceholder.typicode.com","/todos")
+    def var = client.getApiCall(null, "https://jsonplaceholder.typicode.com","/todos")
     //println obj.send_request()
     //println obj.send_request2()
     echo var
