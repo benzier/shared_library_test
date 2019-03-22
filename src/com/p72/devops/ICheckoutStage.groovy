5d2039fef5238e35c47bd0a555b1e777044ef373
Ruben Dopazo <ruben@reluscloud.com>
2019-03-22 18:22:30 -0300

    test

--- src/com/p72/devops/ICheckoutStage.groovy
+++ src/com/p72/devops/ICheckoutStage.groovy
@@ -2,16 +2,6 @@ package com.p72.devops
 
 abstract class ICheckoutStage {
 
-    protected def git
-    protected def sh
-
-    ICheckoutStage(git, sh){
-        this.git = git
-        this.sh = sh
-    }
-    
-    
-
     abstract def checkout(Map params)
 
 }
