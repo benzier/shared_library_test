898105ba05fcef5e0bbb9a4c609e63a423a089d8
Ruben Dopazo <ruben@reluscloud.com>
2019-03-25 11:22:18 -0300

    test

--- src/com/p72/devops/maven/MavenCheckoutStage.groovy
+++ src/com/p72/devops/maven/MavenCheckoutStage.groovy
@@ -2,14 +2,13 @@ package com.p72.devops.maven
 
 import com.p72.devops.*
 
-@groovy.transform.InheritConstructors
+//@groovy.transform.InheritConstructors
 class MavenCheckoutStage extends ICheckoutStage {
 
     //private def pipeline
-    /*MavenCheckoutStage(pipeline){
-        this.pipeline=pipeline
-        pipeline.println(pipeline.class.name)
-    }*/
+    MavenCheckoutStage(jenkins, pipeline){
+        super(jenkins, pipeline)
+    }
 
     /*def checkout(c){
         //pipeline.println pipeline.git.delegate.class.name
