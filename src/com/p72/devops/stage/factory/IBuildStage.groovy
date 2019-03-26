eae60305244f6aa6d9fb331f74427ad126398e41
Ruben Dopazo <ruben@reluscloud.com>
2019-03-26 13:00:09 -0300

    rtest

--- src/com/p72/devops/stage/factory/IBuildStage.groovy
+++ src/com/p72/devops/stage/factory/IBuildStage.groovy
@@ -1,21 +1,4 @@
 package com.p72.devops.stage.factory
 
-import com.p72.devops.util.*
-
-abstract class IBuildStage {
-
-    def pipeline
-    def jenkins
-
-    public IBuildStage(JenkinsUtils jenkins){
-        this.jenkins = jenkins
-    }
-
-    public final void injectPipeline(pipeline){
-        this.pipeline=pipeline
-    }
-
-    abstract def performAction() {}
-    abstract def preAction()
-    abstract def postAction(params)
+interface IBuildStage {
 }
