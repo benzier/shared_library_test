cf5a9e307322aa349d60de5ad7ee952e7f63538d
Ruben Dopazo <ruben@reluscloud.com>
2019-04-01 09:53:29 -0300

    test

--- dsl/test_seed.groovy
+++ dsl/test_seed.groovy
@@ -1,4 +1,13 @@
 pipelineJob('example') {
+    parameters {
+        activeChoiceParam('state') {
+            description('select one option')
+            groovyScript {
+                script('["Sao Paulo", "Rio de Janeiro", "Parana:selected", "Acre"]')
+                fallbackScript('return ["ERROR"]')
+            }
+        }
+    }
     definition {
         cps {
             script('''
