25c0249344eb37ff2f405c2d48ed787183672125
Ruben Dopazo <ruben@reluscloud.com>
2019-04-04 14:12:54 -0300

    asd

--- src/com/p72/devops/util/DelegateSample.groovy
+++ src/com/p72/devops/util/DelegateSample.groovy
@@ -5,9 +5,7 @@ import org.jenkinsci.plugins.workflow.cps.*
 class DelegateSample implements Serializable{
     
     @Delegate
-    DSL pipeline;
-    DelegateSample(DSL p){
-        this.pipeline=p;
-    }
+    DSL pipeline
+    
 
 }
